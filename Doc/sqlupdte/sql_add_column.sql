522182ee902fe628f1366ff913b3a181a1ebf5dd
lxp <lxp>
2016-12-23 21:42:24 +0800

    delete

--- Doc/sqlupdte/sql_add_column.sql
+++
@@ -1,7 +0,0 @@
---修改Locator表，新增webx，weby字段
-alter table Locator add webx int  NULL
-alter table Locator add weby int  NULL
-
---修改CardReader表，新增webx，weby字段
-alter table CardReader add webx int  NULL
-alter table CardReader add weby int  NULL
